a6d951caf223d7c110d35e182285f52c90a5f832
RafaRz76Dev <rafaelraizer76@gmail.com>
2023-06-02 11:48:57 -0300

    resolução-exercicios.js

--- exercicios.js
+++ exercicios.js
@@ -6,8 +6,9 @@
  * @param {number} b - O segundo número.
  * @returns {number} A soma dos dois numeros
  */
+
 function soma(a, b) {
-   return a + b;
+  return a + b;
 }
 let valorsoma = soma(1337, 3301);
 console.log(`A soma dos dois números é => ${valorsoma}.`);
@@ -19,11 +20,12 @@ console.log(`A soma dos dois números é => ${valorsoma}.`);
  * @param {number} b - O segundo número.
  * @returns {number} A multiplicação dos dois numeros
  */
+
 function multiplicacao(a, b) {
-    return a * b;
-  }
-  let valormultiplicacao = multiplicacao(3, 11);
-  console.log(`A multiplicação dos dois números é => ${valormultiplicacao}.`);
+  return a * b;
+}
+let valormultiplicacao = multiplicacao(3, 11);
+console.log(`A multiplicação dos dois números é => ${valormultiplicacao}.`);
 // -------------------------------------
 
 /**
@@ -32,8 +34,9 @@ function multiplicacao(a, b) {
  * @param {number} b - O segundo número.
  * @returns {number} A divisão dos dois numeros
  */
+
 function divisao(a, b) {
-   return a / b;
+  return a / b;
 }
 let valordivisao = divisao(10, 2);
 console.log(`A divisão dos dois números é => ${valordivisao}.`);
@@ -45,8 +48,9 @@ console.log(`A divisão dos dois números é => ${valordivisao}.`);
  * @param {number} b - O segundo número.
  * @returns {number} A subtração dos dois numeros
  */
+
 function subtracao(a, b) {
-    return a - b;
+  return a - b;
 }
 let valorsubtracao = subtracao(3301, 1337);
 console.log(`A subtração dos dois números é => ${valorsubtracao}.`);
@@ -58,11 +62,12 @@ console.log(`A subtração dos dois números é => ${valorsubtracao}.`);
  * @param {number} b - O segundo número.
  * @returns {number} O resto da divisão dos dois numeros
  */
+
 function modulo(a, b) {
-    return a % b;
+  return a % b;
 }
 
-let valormodulo = modulo(15,4);
+let valormodulo = modulo(15, 4);
 console.log(`O resto da divisão dos dois números é => ${valormodulo}.`);
 // -------------------------------------
 
@@ -72,6 +77,7 @@ console.log(`O resto da divisão dos dois números é => ${valormodulo}.`);
  * @param {number} b - O segundo número.
  * @returns {number} O maior número
  */
+
 function max(a, b) {
   if (a > b) {
     return a;
@@ -80,7 +86,8 @@ function max(a, b) {
   }
 }
 let numMax = max(13, 33);
-console.log(`O maior número é => ${numMax}.`);i
+console.log(`O maior número é => ${numMax}.`);
+i;
 // -------------------------------------
 
 /**
@@ -89,6 +96,7 @@ console.log(`O maior número é => ${numMax}.`);i
  * @param {string} b - A segunda string
  * @returns {string} As 2 strings concatenadas
  */
+
 function strConcat(a, b) {
   return `Retorna as Strings ${a} e ${b}.`;
 }
@@ -97,14 +105,15 @@ console.log(strConcat("Hello", "World"));
 
 /**
  * Função que implementa controle de fluxo
- * 
+ *
  * @param {number} numero - O número em questão
- * @returns {string} 
+ * @returns {string}
  *  - "Maior que 10" quando o número é maior que 10
  *  - "Igual a 10" quando o número é igual a 10
  *  - "Menor que 10" quando o número é menor que 10
  */
-  function retornaNumero(numero) {
+
+function retornaNumero(numero) {
   if (numero > 10) {
     console.log(`O número ${numero} é maior do que 10.`);
   } else if (numero < 10) {
@@ -113,6 +122,7 @@ console.log(strConcat("Hello", "World"));
     console.log(`O número ${numero} é igual 10.`);
   }
 }
+
 let valorNumero = retornaNumero(15);
 let valorNumero1 = retornaNumero(10);
 let valorNumero2 = retornaNumero(5);
@@ -123,26 +133,25 @@ let valorNumero2 = retornaNumero(5);
  * @param {number} num - Número em questão
  * @returns {boolean} Retorna true para números positivos
  */
+
 function ehPositivo(num) {
-    if (numero > 0) {
+  if (num > 0) {
     return true;
   } else {
     return false;
   }
 }
+let Numero = ehPositivo(5);
+console.log(Numero);
 
-let valorNumero = ehPositivo(5);
-console.log(valorNumero);
+let Numero1 = ehPositivo(0);
+console.log(Numero1);
 
-let valorNumero1 = ehPositivo(0);
-console.log(valorNumero1);
-
-let valorNumero2 = ehPositivo(-5);
-console.log(valorNumero2);
+let Numero2 = ehPositivo(-5);
+console.log(Numero2);
 // --------------------------------------
-
-module.exports = 
-{ 
+/*
+module.exports = {
   soma,
   multiplicacao,
   divisao,
@@ -151,5 +160,6 @@ module.exports =
   max,
   strConcat,
   retornaNumero,
-  ehPositivo
+  ehPositivo,
 };
+*/
